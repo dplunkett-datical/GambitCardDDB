dfbb91fb34aa43bcc70b90f769935281bff01773
dplunkett@datical.com <dplunkett@datical.com>
2017-05-16 13:37:02 -0500

    firstattmept

---
+++ list_files.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e # fail fast
+set -x # print commands
+
+git clone resource-gambitcardddb gambitcardddb
+
+cd gambitcardddb
+echo $(date) > bumpme
+ls
+
+git config --global user.email "nobody@concourse.ci"
+git config --global user.name "Concourse"
+
+git add .
+git commit -m "Bumped date"
